8a59e7c942d1bce74ee3bf8d35be1b0528fd9f60
krishnaStr <krir@jkhjd>
2026-09-20 23:56:29 +0530

    commited

--- backend/services/auth/controllers/auth.controller.js
+++ backend/services/auth/controllers/auth.controller.js
@@ -41,8 +41,6 @@ export const login = async (req, res) => {
 
         res.cookie("session", sessionId, {
             httpOnly: true,
-            // secure: false,
-            // sameSite: "strict",
             secure: true,
             sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000
